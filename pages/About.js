fe18b5869a542d5341057321565149a24af15cb6
Dhaval <dhavalkshah.2882@gmail.com>
2024-11-28 00:02:25 +0530

    Initial commit from your app

--- pages/About.js
+++ pages/About.js
@@ -1,15 +1,10 @@
 import React from 'react';
-    import Header from '../components/Header';
 
     const About = () => {
       return (
         <div className="container mx-auto">
-          <Header />
-          <main className="py-8">
-            <h1>About Us</h1>
-            <p>This is a simple example of how you can build a website in Next.js using Tailwind CSS.</p>
-          </main>
-          <Footer />
+          <h1>About Us</h1>
+          <p>This is a simple example of how you can build a website in Next.js using Tailwind CSS.</p>
         </div>
       );
     };
